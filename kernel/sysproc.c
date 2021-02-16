6aa97d8d5c691f97f3e85123dd27cb594d99c0dc
young <139397530@qq.com>
2021-02-16 11:39:33 +0800

    eliminate allocation from sbrk()

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -41,14 +41,12 @@ sys_wait(void)
 uint64
 sys_sbrk(void)
 {
-  int addr;
   int n;
-
   if (argint(0, &n) < 0)
     return -1;
-  addr = myproc()->sz;
-  if (growproc(n) < 0)
-    return -1;
+  struct proc *p = myproc();
+  uint64 addr = p->sz;
+  p->sz += n;
   return addr;
 }
 
